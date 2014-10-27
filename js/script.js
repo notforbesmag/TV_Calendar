e4992eaf0ddc5df8126b7b529ea011aed0d12590
Soudea Forbes <soudeaforbes@gmail.com>
2014-10-26 20:52:56 -0400

    giffed
    gifs and video added

--- js/script.js
+++ js/script.js
@@ -22,11 +22,11 @@ $("#submit").click(function() {
     } else if (weekday === "wednesday") {
         $("#answer").html('Catching up on all the shows I missed. Yay <a href="http://www.hulu.com/">Hulu</a>!');
     } else if (weekday === "thursday") {
-        $("#answer").html('<a href="http://www.hulu.com/how-to-get-away-with-murder">How to Get Away with Murder</a>! Shonda Rimes is everything!');
+        $("#answer").html('<a href="http://www.hulu.com/how-to-get-away-with-murder">How to Get Away with Murder</a>! Shonda Rimes is everything!').append('<img src="https://31.media.tumblr.com/4492f5461b6b9bb38372728ce0bb8986/tumblr_ne119xnDYq1sqclzs_500.gif" />');
     } else if (weekday === "friday") {
-        $("#answer").html('<a href="http://www.hulu.com/grimm">Grimm</a> is back... I miss Portland.');
+        $("#answer").html('<a href="http://www.hulu.com/grimm">Grimm</a> is back... I miss Portland.').append('<img src="https://33.media.tumblr.com/03c224f75b27d3a969b07c9466cf7890/tumblr_mjebm5ex281qgapqso1_500.gif" />').append('<img src="https://33.media.tumblr.com/27a40d578864d81c58f4fafa3602234a/tumblr_mjebm5ex281qgapqso2_500.gif" />');
     } else if (weekday === "saturday") {
-        $("#answer").html('<a href="http://www.youtube.com/">Youtube</a>! Cat videos only.');
+        $("#answer").html('<a href="http://www.youtube.com/">Youtube</a>! Cat videos only.').html('<iframe width="420" height="315" src="//www.youtube.com/embed/eiHXASgRTcA" frameborder="0" allowfullscreen></iframe>');
     } else { 
         $("#answer").text("Hmm... Do you need to look at a calendar?");
     }
