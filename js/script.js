3ab0f5189e1bc05c1bcd27706c861cac2f53b14e
Soudea Forbes <soudeaforbes@gmail.com>
2014-10-26 20:34:17 -0400

    linkified fixed
    fixed

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ $("#submit").click(function() {
     if (weekday === "sunday") {
         $("#answer").html('<a href="http://www.hulu.com/once-upon-a-time">Once Upon A Time</a>. So camp yet engaging.');
     } else if (weekday === "monday") {
-        $("#answer").html('No idea. Probably <a href="http://www.netflix.com>"Netflix</a>.');
+        $("#answer").html('No idea. Probably <a href="http://www.netflix.com">Netflix</a>.');
     } else if (weekday === "tuesday") {
         $("#answer").html('<a href="http://www.hulu.com/the-mindy-project">The Mindy Project</a>! Nothing gets between me and Mindy.').append('<img src="https://38.media.tumblr.com/49c3719f73f659cceb2c284080089995/tumblr_ndwzp6ISlL1s9362xo1_500.gif" />');
     } else if (weekday === "wednesday") {
@@ -26,7 +26,7 @@ $("#submit").click(function() {
     } else if (weekday === "friday") {
         $("#answer").html('<a href="http://www.hulu.com/grimm">Grimm</a> is back... I miss Portland.');
     } else if (weekday === "saturday") {
-        $("#answer").html('<a href="http://www.youtube.com/>"Youtube</a>! Cat videos only.'');
+        $("#answer").html('<a href="http://www.youtube.com/">Youtube</a>! Cat videos only.');
     } else { 
         $("#answer").text("Hmm... Do you need to look at a calendar?");
     }
